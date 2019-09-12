294652ab8fe5255082a62d4bc24b7dc917accba6
trankhaiSE <tranvankhai8651@gmail.com>
2019-09-13 01:47:18 +0700

    cau2_c

--- app/src/main/java/com/example/tvk_cau1/MainActivity.java
+++ app/src/main/java/com/example/tvk_cau1/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.tvk_cau1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,16 +25,43 @@ public class MainActivity extends AppCompatActivity {
         editPass = (EditText) findViewById(R.id.Password);
         btndangnhap = (Button) findViewById(R.id.Commit);
         checkLuu = (CheckBox) findViewById(R.id.Checked);
+        btnThoat = (Button) findViewById(R.id.Exit);
 
         btndangnhap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (checkLuu.isChecked()) {
-                    Toast.makeText(MainActivity.this, "Chào m?ng b?n dang nh?p h? th?ng, thông tin c?a b?n dã du?c luu", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Chào mừng bạn đăng nhập hệ thống, thông tin của bạn đã được lưu", Toast.LENGTH_SHORT).show();
                 } else
-                    Toast.makeText(MainActivity.this, "Chào m?ng b?n dang nh?p h? th?ng, thông tin c?a b?n không du?c luu", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Chào mừng bạn đăng nhập hệ thống, thông tin của bạn không được lưu", Toast.LENGTH_LONG).show();
             }
         });
-
+        btnThoat.setOnClickListener(new View.OnClickListener(){
+            @Override
+                public void onClick(View view){
+                    showAlertDialog();
+                }
+        });
     }
+    public void showAlertDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Thông báo");
+        builder.setMessage("B?n mu?n thoát không");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Không", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
 }
